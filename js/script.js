5131a224f7681ca0b001ae5fb7c8bdf885914838
ZhouYunzhi18 <36897621+ZhouYunzhi18@users.noreply.github.com>
2018-03-16 11:37:23 +0100

    week3

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function switchChannela() {
+    $('#chatbardisplay').html("#Yummy");
+    $('#locationdisplay').html("upgrading.never.helps");
+}
+
+
+function switchChannelb() {
+    $('#chatbardisplay').html("#SevenContinents");
+    $('#locationdisplay').html("upgrading.never.helps");
+}
+
+
+function switchChannelc() {
+    $('#chatbardisplay').html("#KillerApp");
+    $('#locationdisplay').html("upgrading.never.helps");
+}
+
+
+function switchChanneld() {
+    $('#chatbardisplay').html("#FirstPersonOnMars");
+    $('#locationdisplay').html("upgrading.never.helps");
+}
+
+
+function switchChannele() {
+    $('#chatbardisplay').html("#Octoberfest");
+    $('#locationdisplay').html("upgrading.never.helps" );
+}
+
+function change() {
+  // Change the inner HTML of the date display div
+  $('#filled').hide();
+}
+function changea() {
+  // Change the inner HTML of the date display div
+  $('#filled').show();
+}
+function changeb() {
+  // Change the inner HTML of the date display div
+  $('#emojis').hide();
+    toggleButtons();
+}
+function toggleButtons() {
+  $('emojis-button').toggle();  
+}
